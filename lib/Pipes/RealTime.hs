b9a2fb3b0bc0e33350e33a1434c8c00cfdbd224f
Greg Hale <imalsogreg@gmail.com>
2013-11-06 22:15:39 -0500

    removed typeclasses

--- lib/Pipes/RealTime.hs
+++ lib/Pipes/RealTime.hs
@@ -1,9 +1,5 @@
 module Pipes.RealTime (
 
-  -- *Classes for data with attached timestamps
-  TimedEvent(..),
-  TMinus(..),
-
   -- *Pipes throttled by their own timestamps
   timeCat,
   relativeTimeCat,
@@ -31,52 +27,41 @@ import System.Random
 import Statistics.Distribution
 import Statistics.Distribution.Exponential
 
-{-| Values in TimedEvents can produce a UTCTime, which is the time
-    at which the value should be yielded -}
-class TimedEvent a where
-  timeOf :: a -> UTCTime
-
-{-| Values in TMinus are values that produce a relative-time, which
-    is the time passed (in seconds) since the effect was run at which
-    the value will be yielded -}
-class TMinus a where
-  tMinusSec :: a -> Double
-
 {-| Yield values some time after the effect is run,
     according to their relative timestamps.  Assumes that
     values arrive in ascending time order -}
-relativeTimeCat :: (TMinus a) => Pipe a a IO r
-relativeTimeCat = do
+relativeTimeCat :: (a -> Double) -> Pipe a a IO r
+relativeTimeCat toTime = do
   t0 <- lift getCurrentTime
   forever $ do
     v <- await
-    lift $ pauseUntil (doubleToNomDiffTime (tMinusSec v) `addUTCTime` t0)
+    lift $ pauseUntil (doubleToNomDiffTime (toTime v) `addUTCTime` t0)
     yield v
 
 {-| Yield values at the absolute times given by their timestamps.
     Assumes that they arrive in ascending time order. Values with timestamps
     earlier than the starting time of the effect are yielded immediately -}
-timeCat :: (TimedEvent a) => Pipe a a IO r
-timeCat = forever $ do
+timeCat :: (a -> UTCTime) -> Pipe a a IO r
+timeCat toRelTime = forever $ do
     v <- await
-    lift $ pauseUntil (timeOf v)
+    lift $ pauseUntil (toRelTime v)
     yield v
 
 {-| Discard events whose timestamps occur before the effect started running,
     instead of yielding them -}
-dropExpired :: (TimedEvent a) => Pipe a a IO ()
-dropExpired = do
+dropExpired :: (a -> UTCTime) -> Pipe a a IO ()
+dropExpired toTime = do
   v <- await
   now <- lift getCurrentTime
-  case compare now (timeOf v) of
-    GT -> dropExpired
+  case compare now (toTime v) of
+    GT -> dropExpired toTime
     _  -> return ()
 
 {-| Discard events whose relative timestamps are less than 0 -}
-dropRelativeExpired :: (Monad m, TMinus a) => Pipe a a m ()
-dropRelativeExpired = do
+dropRelativeExpired :: (Monad m) => (a -> Double) -> Pipe a a m ()
+dropRelativeExpired toRelTime = do
   v <- await
-  when (tMinusSec v < 0) dropRelativeExpired
+  when (toRelTime v < 0) (dropRelativeExpired toRelTime)
   
 {-| Yield values at stead rate (Hz) -}
 steadyCat :: Double -> Pipe a a IO r
